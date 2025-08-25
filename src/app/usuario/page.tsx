69d112371c75c9fe5136dbf3c621a1d2a659f56f
Rian Landim <rianlandim222@gmail.com>
2025-08-25 19:55:28 -0300

    build: add the shadcn and tailwind configuration on repo

---
+++ src/app/usuario/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "~/components/ui/button";
+
+export default function UserPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button>Teste</Button>
+    </div>
+  );
+}
